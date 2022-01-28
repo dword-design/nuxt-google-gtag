42c2c3a479f8b91233e106468b276f429cf3bbf2
Sebastian Landwehr <info@dword-design.de>
2022-01-28 22:13:07 +0100

    fix: foo

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import packageName from 'depcheck-package-name'
 
 export default function (options) {
-  return this.addModule(packageName`@nuxtjs/google-gtag`, {
-    config: {
-      anonymize_ip: true,
-      ...options.config,
+  return this.addModule([
+    packageName`@nuxtjs/google-gtag`,
+    {
+      config: {
+        anonymize_ip: true,
+        ...options.config,
+      },
+      debug: true,
+      id: process.env.GOOGLE_ANALYTICS_ID,
+      ...options,
     },
-    debug: true,
-    id: process.env.GOOGLE_ANALYTICS_ID,
-    ...options,
-  })
+  ])
 }
